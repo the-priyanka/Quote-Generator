0fec2c985fd3fd12d57dda098bcac00ccd8f0b83
Priyanka <original.manaria@gmail.com>
2021-08-27 16:49:49 +0530

    comlete this project

--- js/script.js
+++ js/script.js
@@ -2,10 +2,25 @@ const quoteContainer = document.getElementById("quote-container")
 const quoteText = document.getElementById("quote")
 const authorText = document.getElementById("author")
 const newQuoteBtn = document.getElementById("new-quote")
+const twitter = document.getElementById("twitter")
+const loader = document.getElementById("loader")
 
 let apiQuotes = []
 
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hidden loading 
+function complete() {
+  quoteContainer.hidden = false
+  loader.hidden = true
+}
+
 function newQuote() {
+  loading()
   let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   // console.log(quote)
 
@@ -18,16 +33,18 @@ function newQuote() {
   }
 
   // text code 
-  if(quote.text.length > 120){
+  if (quote.text.length > 120) {
     quoteText.classList.add('long-quote')
   }
-  else{
+  else {
     quoteText.classList.remove('long-quote')
   }
   quoteText.textContent = quote.text
+  complete()
 }
 
 async function getQuote() {
+  loading()
   const apiUrl = "https://type.fit/api/quotes"
 
   try {
@@ -42,5 +59,15 @@ async function getQuote() {
   }
 }
 
+// tweeter quote 
+function tweetQuote() {
+  let quote = quoteText.innerText
+  let author = authorText.innerText
+  let tweeterUrl = `https://twitter.com/intent/tweet?text=${ quote } - ${ author }`
+  window.open(tweeterUrl, "_blank")
+}
+
 newQuoteBtn.addEventListener('click', newQuote)
+twitter.addEventListener('click', tweetQuote)
+
 getQuote()
